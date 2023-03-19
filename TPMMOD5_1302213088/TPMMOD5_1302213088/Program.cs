87724e297b4e5a46c0ee6ce32d4d1397b176f8ba
mamanGod <127042964+mamanGod@users.noreply.github.com>
2023-03-19 23:27:50 +0700

    menambahkan class DataGeneric

--- TPMMOD5_1302213088/TPMMOD5_1302213088/Program.cs
+++ TPMMOD5_1302213088/TPMMOD5_1302213088/Program.cs
@@ -1,2 +1,25 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+public class DataGeneric<X>
+{
+    private X data;
+
+    public DataGeneric(X data)
+    {
+        this.data = data;
+    }
+
+    public void PrintData()
+    {
+        Console.WriteLine("Data yang tersimpan adalah: " + data.ToString());
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        DataGeneric<string> data = new DataGeneric<string>("1302213088");
+        data.PrintData();
+    }
+}
